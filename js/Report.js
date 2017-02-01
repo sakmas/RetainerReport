df916c1add8cc521539a076a099f41173bf08f0b
sakmas <sakmas@users.noreply.github.com>
2017-02-02 00:52:37 +0900

    Add HQ icon to item name

--- js/Report.js
+++ js/Report.js
@@ -1,6 +1,6 @@
 module.exports = (function() {
   return function($record) {
-    this.name = $record.find('.retainer_item_txt').text().trim();
+    this.name = $record.find('.retainer_item_txt').html();
     var _info = $record.children('td');
     this.salePrice = _info.eq(0).text().trim();
     this.buyer = _info.eq(1).text().trim();
