b7247066e55fc83f51e753aab16fe29784d37584
sakmas <sakmas@users.noreply.github.com>
2018-04-30 15:20:49 +0900

    Replace '本日' with date string

--- js/Report.js
+++ js/Report.js
@@ -1,9 +1,20 @@
 module.exports = (function() {
+  function buildDate(match, p1) {
+    var d = new Date();
+    var date = [
+      d.getFullYear(),
+      ('0' + (d.getMonth() + 1)).substr(-2),
+      ('0' + d.getDate()).substr(-2)
+    ].join('/');
+    return [date, ' ', ('0'+p1).substr(-2), ':'].join('');
+  }
+
   return function($record) {
     this.name = $record.find('.item-list__name').html();
     var _info = $record.children('div');
     this.salePrice = _info.eq(1).text().trim();
     this.buyer = _info.eq(2).text().trim();
-    this.saleDate = eval(_info.eq(3).children('script').text().split(' = ')[1]);
+    var _saleDate = eval(_info.eq(3).children('script').text().split(' = ')[1]);
+    this.saleDate = _saleDate.replace(/本日 (\d+):/, buildDate);
   };
 })();
